74123baeeb7f66bc142c6b7a51d01e8de0145c6b
berlianafd <berlianafd@gmail.com>
2020-05-06 01:47:28 +0700

    Reset semua. Mulai dari awal

--- app/src/main/java/com/berlianafd/kalkulator/MainActivity.java
+++ app/src/main/java/com/berlianafd/kalkulator/MainActivity.java
@@ -1,324 +1,203 @@
 package com.berlianafd.kalkulator;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.math.MathContext;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import androidx.core.content.res.ResourcesCompat;
+
 public class MainActivity extends Activity {
     double nilai_oqf=0.249, nilai_ff=0.397, nilai_pcf=0.357;
-    EditText wf,wp;
-    TextView resultt;
-    ImageButton del_input_factor, del_input_param;
-    Button ac_input_factor, oqf, ff, pcf;
-    Button bdv, water, acid, ift, col, ddf, sed;
-    Button h2, ch4, c2h4, c2h6, c2h2;
-    Button coco2, fal, age, dp;
-    LinearLayout input_factor, input_param_oq, param_faults, param_paper;
+
+    private DatePickerDialog datePickerDialog;
+    private SimpleDateFormat dateFormatter;
+
+    RelativeLayout button_trafoId, button_dc, button_dg, button_furan, button_dpk;
+    ImageView arrow_trafoId, arrow_dc, arrow_dg, arrow_furan, arrow_dpk;
+    LinearLayout konten_trafoId, konten_dg, konten_dc, konten_furan, konten_dpk;
+
+    EditText meaDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        wf = (EditText) findViewById(R.id.wf);
-        wp = (EditText) findViewById(R.id.wp);
-        resultt = (TextView) findViewById(R.id.result);
-        input_factor = (LinearLayout) findViewById(R.id.inputfactor);
-        input_param_oq = (LinearLayout) findViewById(R.id.parameter_oil_quality);
-        param_faults = (LinearLayout) findViewById(R.id.parameter_faults);
-        param_paper = (LinearLayout) findViewById(R.id.parameter_paper);
-//        button input factor
-        ac_input_factor = (Button) findViewById(R.id.ac_input_factor);
-        del_input_factor = (ImageButton) findViewById(R.id.del_input_factor);
-        del_input_param = (ImageButton) findViewById(R.id.del_input_param);
-        oqf = (Button) findViewById(R.id.oqf);
-        ff = (Button) findViewById(R.id.ff);
-        pcf = (Button) findViewById(R.id.pcf);
+        dateFormatter = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
 
-//        button input parameter oil
-        bdv = (Button) findViewById(R.id.bdv_param_oq);
-        water = (Button) findViewById(R.id.water_param_oq);
-        acid = (Button) findViewById(R.id.acid_param_oq);
-        ift = (Button) findViewById(R.id.ift_param_oq);
-        col = (Button) findViewById(R.id.col_param_oq);
-        ddf = (Button) findViewById(R.id.ddf_param_oq);
-        sed = (Button) findViewById(R.id.sed_param_oq);
+        button_trafoId = (RelativeLayout) findViewById(R.id.button_trafoId);
+        button_dc = (RelativeLayout) findViewById(R.id.button_dc);
+        button_dg = (RelativeLayout) findViewById(R.id.button_dg);
+        button_furan = (RelativeLayout) findViewById(R.id.button_furan);
+        button_dpk = (RelativeLayout) findViewById(R.id.button_dpk);
 
-        //button input parameter faults
-        h2 = (Button) findViewById(R.id.h2_faults);
-        c2h2 = (Button) findViewById(R.id.c2h2_faults);
-        c2h4 = (Button) findViewById(R.id.c2h4_faults);
-        c2h6 = (Button) findViewById(R.id.c2h6_faults);
-        ch4 = (Button) findViewById(R.id.ch4_faults);
+        arrow_trafoId = (ImageView) findViewById(R.id.arrow_trafoId);
+        arrow_dc = (ImageView) findViewById(R.id.arrow_dc);
+        arrow_dg = (ImageView) findViewById(R.id.arrow_dg);
+        arrow_furan = (ImageView) findViewById(R.id.arrow_furan);
+        arrow_dpk = (ImageView) findViewById(R.id.arrow_dpk);
 
-//        button input parameter paper
-        coco2 = (Button) findViewById(R.id.coco2_paper);
-        fal = (Button) findViewById(R.id.fal_paper);
-        age = (Button) findViewById(R.id.age_paper);
-        dp = (Button) findViewById(R.id.dp_paper);
+        konten_trafoId = (LinearLayout) findViewById(R.id.konten_trafoId);
+        konten_dc = (LinearLayout) findViewById(R.id.konten_dc);
+        konten_dg = (LinearLayout) findViewById(R.id.konten_dg);
+        konten_furan = (LinearLayout) findViewById(R.id.konten_furan);
+        konten_dpk = (LinearLayout) findViewById(R.id.konten_dpk);
 
-
-        wf.addTextChangedListener(new TextWatcher() {
-            public void afterTextChanged(Editable s) {
-
-                if (s.length()>0) {
-                    wp.requestFocus();
-                    del_input_factor.setVisibility(View.GONE);
-                    del_input_param.setVisibility(View.VISIBLE);
+        button_trafoId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(konten_trafoId.getVisibility() == View.VISIBLE){
+                    konten_trafoId.setVisibility(View.GONE);
+                    arrow_trafoId.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.arrow_bottom, null));
+                } else{
+                    konten_trafoId.setVisibility(View.VISIBLE);
+                    arrow_trafoId.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.arrow_up, null));
                 }
-
-            }
-
-            public void beforeTextChanged(CharSequence s, int start, int count,
-                                          int after) {
-                wf.requestFocus();
-            }
-
-            public void onTextChanged(CharSequence s, int start, int before,
-                                      int count) {
             }
         });
-
-//        klik on button input factor
-        ac_input_factor.setOnClickListener(new View.OnClickListener() {
+        button_dg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                wf.setText("");
-                wp.setText("");
-                resultt.setText("");
-                input_factor.setVisibility(View.VISIBLE);
-                input_param_oq.setVisibility(View.GONE);
-                param_faults.setVisibility(View.GONE);
-                param_paper.setVisibility(View.GONE);
+                if(konten_dg.getVisibility() == View.VISIBLE){
+                    konten_dg.setVisibility(View.GONE);
+                    arrow_dg.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.arrow_bottom, null));
+                } else{
+                    konten_dg.setVisibility(View.VISIBLE);
+                    arrow_dg.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.arrow_up, null));
+                }
             }
         });
-
-        wf.setOnClickListener(new View.OnClickListener() {
+        button_dc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                del_input_factor.setVisibility(View.VISIBLE);
-                del_input_param.setVisibility(View.GONE);
+                if(konten_dc.getVisibility() == View.VISIBLE){
+                    konten_dc.setVisibility(View.GONE);
+                    arrow_dc.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.arrow_bottom, null));
+                } else{
+                    konten_dc.setVisibility(View.VISIBLE);
+                    arrow_dc.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.arrow_up, null));
+                }
             }
         });
-
-        del_input_factor.setOnClickListener(new View.OnClickListener() {
+        button_furan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                wf.setText("");
-                resultt.setText("");
-                input_factor.setVisibility(View.VISIBLE);
-                input_param_oq.setVisibility(View.GONE);
-                param_faults.setVisibility(View.GONE);
-                param_paper.setVisibility(View.GONE);
-
-                if(!wp.getText().equals("")){
-                    wp.setText("");
-                    wf.requestFocus();
+                if(konten_furan.getVisibility() == View.VISIBLE){
+                    konten_furan.setVisibility(View.GONE);
+                    arrow_furan.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.arrow_bottom, null));
+                } else{
+                    konten_furan.setVisibility(View.VISIBLE);
+                    arrow_furan.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.arrow_up, null));
+                }
+            }
+        });
+        button_dpk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(konten_dpk.getVisibility() == View.VISIBLE){
+                    konten_dpk.setVisibility(View.GONE);
+                    arrow_dpk.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.arrow_bottom, null));
+                } else{
+                    konten_dpk.setVisibility(View.VISIBLE);
+                    arrow_dpk.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.arrow_up, null));
                 }
             }
         });
 
-        wp.setOnClickListener(new View.OnClickListener() {
+        meaDate = (EditText) findViewById(R.id.input_meaDate);
+
+        meaDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                del_input_factor.setVisibility(View.GONE);
-                del_input_param.setVisibility(View.VISIBLE);
-
-                if (wf.getText().length()==0){
-                    Toast.makeText(getApplicationContext(), "Factor can't be blank!",
-                            Toast.LENGTH_LONG).show();
-                }
+                showDateDialog();
             }
         });
 
-        del_input_param.setOnClickListener(new View.OnClickListener() {
+        double a = ((1.0/200)-(1.0/1057.13))/(0.67*Math.pow(10,8)*24*365);
+        double b = Math.exp((111000.0/(8.314*(30+68+273))));
+        double ab = a*b;
+
+
+//        wf.addTextChangedListener(new TextWatcher() {
+//            public void afterTextChanged(Editable s) {
+//
+//                if (s.length()>0) {
+//                    wp.requestFocus();
+//                    del_input_factor.setVisibility(View.GONE);
+//                    del_input_param.setVisibility(View.VISIBLE);
+//                }
+//
+//            }
+//
+//            public void beforeTextChanged(CharSequence s, int start, int count,
+//                                          int after) {
+//                wf.requestFocus();
+//            }
+//
+//            public void onTextChanged(CharSequence s, int start, int before,
+//                                      int count) {
+//
+//            }
+//        });
+
+
+    }
+
+    private void showDateDialog(){
+        /**
+         * Calendar untuk mendapatkan tanggal sekarang
+         */
+        Calendar newCalendar = Calendar.getInstance();
+
+        /**
+         * Initiate DatePicker dialog
+         */
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
             @Override
-            public void onClick(View view) {
-                wp.setText("");
-                resultt.setText("");
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                /**
+                 * Method ini dipanggil saat kita selesai memilih tanggal di DatePicker
+                 */
+
+                /**
+                 * Set Calendar untuk menampung tanggal yang dipilih
+                 */
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+
+                /**
+                 * Update TextView dengan tanggal yang kita pilih
+                 */
+                meaDate.setText(dateFormatter.format(newDate.getTime()));
             }
-        });
 
-        oqf.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wf.setText("OQF");
-                input_factor.setVisibility(View.GONE);
-                input_param_oq.setVisibility(View.VISIBLE);
-                param_faults.setVisibility(View.GONE);
-                param_paper.setVisibility(View.GONE);
-            }
-        });
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
 
-        ff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wf.setText("FF");
-                input_factor.setVisibility(View.GONE);
-                input_param_oq.setVisibility(View.GONE);
-                param_faults.setVisibility(View.VISIBLE);
-                param_paper.setVisibility(View.GONE);
-            }
-        });
-
-        pcf.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wf.setText("PCF");
-                input_factor.setVisibility(View.GONE);
-                input_param_oq.setVisibility(View.GONE);
-                param_faults.setVisibility(View.GONE);
-                param_paper.setVisibility(View.VISIBLE);
-            }
-        });
-
-//        klik on button parameter oil
-        bdv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wp.setText("BDV");
-                resultt.setText(Double.toString(hasil(nilai_oqf, 0.148)));
-
-            }
-        });
-
-        water.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wp.setText("WATER");
-                resultt.setText(Double.toString(hasil(nilai_oqf, 0.097)));
-
-            }
-        });
-
-        acid.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wp.setText("ACID");
-                resultt.setText(Double.toString(hasil(nilai_oqf, 0.136)));
-
-            }
-        });
-
-        ift.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wp.setText("IFT");
-                resultt.setText(Double.toString(hasil(nilai_oqf, 0.128)));
-
-            }
-        });
-
-        col.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wp.setText("COL");
-                resultt.setText(Double.toString(hasil(nilai_oqf, 0.126)));
-
-            }
-        });
-
-        ddf.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wp.setText("DDF");
-                resultt.setText(Double.toString(hasil(nilai_oqf, 0.251)));
-
-            }
-        });
-
-        sed.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wp.setText("SED");
-                resultt.setText(Double.toString(hasil(nilai_oqf, 0.115)));
-
-            }
-        });
-
-        //klik on button faults
-        h2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wp.setText("H2");
-                resultt.setText(Double.toString(hasil(nilai_ff, 2)));
-
-            }
-        });
-        ch4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wp.setText("CH4");
-                resultt.setText(Double.toString(hasil(nilai_ff, 3)));
-
-            }
-        });
-        c2h4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wp.setText("C2H4");
-                resultt.setText(Double.toString(hasil(nilai_ff, 3)));
-
-            }
-        });
-        c2h6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wp.setText("C2H6");
-                resultt.setText(Double.toString(hasil(nilai_ff, 3)));
-
-            }
-        });
-        c2h2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wp.setText("C2H2");
-                resultt.setText(Double.toString(hasil(nilai_ff, 5)));
-
-            }
-        });
-
-//        klik on button parameter paper
-        coco2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wp.setText("COCO2");
-                resultt.setText(Double.toString(hasil(nilai_pcf, 0.203)));
-
-            }
-        });
-        fal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wp.setText("2FAL");
-                resultt.setText(Double.toString(hasil(nilai_pcf, 0.261)));
-
-            }
-        });
-        age.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wp.setText("AGE");
-                resultt.setText(Double.toString(hasil(nilai_pcf, 0.242)));
-
-            }
-        });
-        dp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wp.setText("DP");
-                resultt.setText(Double.toString(hasil(nilai_pcf, 0.294)));
-
-            }
-        });
+        /**
+         * Tampilkan DatePicker dialog
+         */
+        datePickerDialog.show();
     }
 
     public double hasil(double wf, double wp){
